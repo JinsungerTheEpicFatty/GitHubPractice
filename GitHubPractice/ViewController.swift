01c6f18101891d7998fc7c46e2f4e88cdc79e75a
Alisaad Khowaja <akhowaja2201@stu.d214.org>
2022-04-04 14:44:50 -0500

    put in multiplying code

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var answerLabel : UILabel!
     override func viewDidLoad() {
+       
+ 
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func whenAddButtonPressed(_sender:UIButton) {
+
+        let data = firstNumberTextField.text!
+        let number1 = Int(firstNumberTextField.text!)!
+        let number2 = Int(secondNumberTextField.text!)!
+        let answer = number1 * number2
+        answerLabel.text = "\(answer)"
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+    
+    }
 
 }
-
+}
