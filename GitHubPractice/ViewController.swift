e07f8795c843e1f76f018277950550724a7b2f9c
Alisaad Khowaja <akhowaja2201@stu.d214.org>
2022-04-04 14:48:47 -0500

    made a correction

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         let data = firstNumberTextField.text!
         let number1 = Int(firstNumberTextField.text!)!
         let number2 = Int(secondNumberTextField.text!)!
-        let answer = number1 * number2
+        let answer = number1 + number2
         answerLabel.text = "\(answer)"
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             textField.resignFirstResponder()
